ba4be16fc00b4bb71fcdfd7799191f885f439298
Alex Mospan <Alexander.mospan@wahroonga.adventist.edu.au>
2026-06-15 11:15:46 +1000

    Start to building webpage

---
+++ main.py
@@ -0,0 +1,11 @@
+from flask import Flask, render_template, request, jsonify, session, redirect, send_from_directory
+
+app = Flask(__name__)
+
+# Define the route for the homepage
+@app.route("/") 
+def Home(): 
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
